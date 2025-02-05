bf20b64d0035f4d9c23a4ba14a78827812c58ed3
Russell Samora <russellsamora@gmail.com>
2025-02-05 14:44:47 -0500

    update github pages

--- docs/_app/immutable/chunks/index-client.BEKBaQMh.js
+++
@@ -1,1 +0,0 @@
-import{c as t,l as e,u as c,a as u}from"./runtime.B-x8vvAX.js";function a(o){t===null&&e(),c(()=>{const n=u(o);if(typeof n=="function")return n})}export{a as o};
---
+++ docs/_app/immutable/entry/start.gYzuXzAo.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.-Nnda3Qp.js";export{t as start};
